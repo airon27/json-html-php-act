2266680dbd09330a698983764aadbe44044d9431
airon27 <aironprincebeltran27@gmail.com>
2023-04-05 17:51:05 +0800

    Commit: data inputted

---
+++ socmed.php
@@ -0,0 +1,51 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    // root
+    $socmed = [
+        [   
+            "img" => "img",
+            "name" => "Facebook",
+            "company" => "Meta Platforms, Inc.",
+            "launched" => "February 4, 2004",
+            "description" => "Facebook is the largest social media platform in the world.",
+            "founders" => ["Mark Zuckerberg","Eduardo Saverin","Dustin Moskovitz","Chris Hughes"]
+        ],
+        [
+            "img" => "img",
+            "name" => "Youtube",
+            "company" => "Google Inc.",
+            "launched" => "February 14, 2005",
+            "description" => "YouTube dominates video content sharing.",
+            "founders" => ["Steve Chen","Chad Hurley","Jawed Karim","Susan Diane Wojcicki"]
+        ],
+        [
+            "img" => "img",
+            "name" => "Instagram",
+            "company" => "Meta Platforms, Inc.",
+            "launched" => "October 6, 2010",
+            "description" => "Instagram is a social network where product-based businesses, influencers, and coaches can thrive.",
+            "founders" => ["Kevin Systrom","Mike Krieger","Adam","Mosseri"]
+        ],
+        [
+            "img" => "img",
+            "name" => "Tiktok",
+            "company" => "ByteDance Ltd.",
+            "launched" => "September 2016",
+            "description" => "Tiktok is the leading destination for short-form mobile video” with a company mission to inspire creativity and bring joy.",
+            "founders" => ["Zhang Yiming","Toutiao","Shou","Zi Chew"]
+        ],
+        [
+            "img" => "img",
+            "name" => "Twitter",
+            "company" => "Twitter, Inc.",
+            "launched" => "March 21, 2006",
+            "description" => "Twitter is a microblogging platform where users primarily share short posts called tweets.",
+            "founders" => ["Evan Williams", "Biz Stone","Noah Glass","Jack Dorsey"]
+        ]
+
+    ];
+
+    $data = json_encode($socmed);
+    echo $data;
+    
+?>
